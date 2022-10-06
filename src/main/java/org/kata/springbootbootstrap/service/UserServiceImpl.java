b2432e132c71a1c15bb037078f0b62c4a96602b7
Вадим Дмитриенко <91070573+ErikDorn@users.noreply.github.com>
2022-10-06 10:21:10 +0300

    Update UserServiceImpl.java

--- src/main/java/org/kata/springbootbootstrap/service/UserServiceImpl.java
+++ src/main/java/org/kata/springbootbootstrap/service/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package org.kata.springbootbootstrap.service;
 
 import org.kata.springbootbootstrap.model.User;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,60 +9,70 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserDetailsService {
-    private final UserQuerySQL userQuerySQL;
+    private final UserDAO userDAO;
     private final PasswordEncoder passwordEncoder;
+    private final RoleDAO roleDAO;
 
     @Autowired
     @Lazy
-    public UserServiceImpl(UserQuerySQL userQuerySQL, PasswordEncoder passwordEncoder) {
-        this.userQuerySQL = userQuerySQL;
+    public UserServiceImpl(RoleDAO roleDAO, UserDAO userDAO, PasswordEncoder passwordEncoder) {
+        this.userDAO = userDAO;
         this.passwordEncoder = passwordEncoder;
+        this.roleDAO = roleDAO;
     }
 
 
     @Transactional
     public void saveUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userQuerySQL.saveUser(user);
+        userDAO.saveUser(user);
     }
 
 
     @Transactional
     public void updateUser(User user) {
 
-        if (!user.getPassword().equals(userQuerySQL.getUserById(user.getId()).getPassword())) {
+        if (!user.getPassword().equals(userDAO.getUserById(user.getId()).getPassword())) {
             user.setPassword(passwordEncoder.encode(user.getPassword()));
         }
-        userQuerySQL.updateUser(user);
+        userDAO.updateUser(user);
     }
 
 
     @Transactional
     public void deleteUser(long id) {
-        userQuerySQL.deleteUser(id);
+        userDAO.deleteUser(id);
     }
 
 
     public List<User> getAllUsers() {
-        return userQuerySQL.getAllUsers();
+        return userDAO.getAllUsers();
     }
 
 
     public User getUserByLogin(String email) {
-        return userQuerySQL.getUserByEmail(email);
+        return userDAO.getUserByEmail(email);
     }
 
 
     public User getUserById(long id) {
-        return userQuerySQL.getUserById(id);
+        return userDAO.getUserById(id);
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userQuerySQL.getUserByEmail(email);
+        return userDAO.getUserByEmail(email);
+    }
+
+    public void getUserRoles(User user) {
+        user.setRoles(user.getRoles().stream()
+                .map(role -> roleDAO.getRole(role.getName()))
+                .collect(Collectors.toSet()));
     }
 }
