597ff2ec4deecd11ee4aaf1a4ce6f67ff7f2e4a6
Вадим Дмитриенко <91070573+ErikDorn@users.noreply.github.com>
2022-10-06 10:19:45 +0300

    Update RoleServiceImpl.java

--- src/main/java/org/kata/springbootbootstrap/service/RoleServiceImpl.java
+++ src/main/java/org/kata/springbootbootstrap/service/RoleServiceImpl.java
@@ -5,35 +5,37 @@ import org.kata.springbootbootstrap.model.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
 public class RoleServiceImpl {
-    private final RoleQuerySQL roleQuerySQL;
+    private final RoleDAO roleDAO;
 
     @Autowired
-    public RoleServiceImpl(RoleQuerySQL roleQuerySQL) {
-        this.roleQuerySQL = roleQuerySQL;
+    public RoleServiceImpl(RoleDAO roleDAO) {
+        this.roleDAO = roleDAO;
     }
 
 
     public List<Role> getAllRoles() {
-        return roleQuerySQL.getAllRoles();
+        return roleDAO.getAllRoles();
     }
 
 
     public Role getRole(String userRole) {
-        return roleQuerySQL.getRole(userRole);
+        return roleDAO.getRole(userRole);
     }
 
 
     public Role getRoleById(Long id) {
-        return roleQuerySQL.getRoleById(id);
+        return roleDAO.getRoleById(id);
     }
 
 
     @Transactional
     public void addRole(Role role) {
-        roleQuerySQL.addRole(role);
+        roleDAO.addRole(role);
     }
 }
+
